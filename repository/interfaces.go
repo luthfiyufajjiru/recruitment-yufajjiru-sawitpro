0a3a288883d4601b922b2bd78aa0f3799a5ed2ea
yufajjiru-dev <luthfiyufajjiru@gmail.com>
2024-03-30 04:17:39 +0700

    feat: define repository abstraction

--- repository/interfaces.go
+++ repository/interfaces.go
@@ -1,11 +1,17 @@
-// This file contains the interfaces for the repository layer.
-// The repository layer is responsible for interacting with the database.
-// For testing purpose we will generate mock implementations of these
-// interfaces using mockgen. See the Makefile for more information.
 package repository
 
-import "context"
+import (
+	"context"
+
+	"github.com/golang-jwt/jwt/v5"
+)
 
 type RepositoryInterface interface {
-	GetTestById(ctx context.Context, input GetTestByIdInput) (output GetTestByIdOutput, err error)
+	UpdateProfile(ctx context.Context, user_id int) (err error)
+	GetProfile(ctx context.Context, user_id int) (err error)
+	ComparePassword(ctx context.Context, phone_number, password string) (err error)
+	SignJWTToken(cl jwt.MapClaims, key []byte) (tokenStr string, err error)
+	GenJWTTokens(user_id int, name string) (accessTokenStr, refreshTokenStr string, err error)
+	GetClaims(tokenStr string, key []byte) (claims jwt.MapClaims, valid bool)
+	RefreshToken(token string) (accesstoken string, err error)
 }
--- repository/interfaces.mock.gen.go
+++ repository/interfaces.mock.gen.go
@@ -8,6 +8,7 @@ import (
 	context "context"
 	reflect "reflect"
 
+	v5 "github.com/golang-jwt/jwt/v5"
 	gomock "github.com/golang/mock/gomock"
 )
 
@@ -34,6 +35,65 @@ func (m *MockRepositoryInterface) EXPECT() *MockRepositoryInterfaceMockRecorder 
 	return m.recorder
 }
 
+// ComparePassword mocks base method.
+func (m *MockRepositoryInterface) ComparePassword(ctx context.Context, phone_number, password string) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "ComparePassword", ctx, phone_number, password)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// ComparePassword indicates an expected call of ComparePassword.
+func (mr *MockRepositoryInterfaceMockRecorder) ComparePassword(ctx, phone_number, password interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ComparePassword", reflect.TypeOf((*MockRepositoryInterface)(nil).ComparePassword), ctx, phone_number, password)
+}
+
+// GenJWTTokens mocks base method.
+func (m *MockRepositoryInterface) GenJWTTokens(user_id int, name string) (string, string, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GenJWTTokens", user_id, name)
+	ret0, _ := ret[0].(string)
+	ret1, _ := ret[1].(string)
+	ret2, _ := ret[2].(error)
+	return ret0, ret1, ret2
+}
+
+// GenJWTTokens indicates an expected call of GenJWTTokens.
+func (mr *MockRepositoryInterfaceMockRecorder) GenJWTTokens(user_id, name interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GenJWTTokens", reflect.TypeOf((*MockRepositoryInterface)(nil).GenJWTTokens), user_id, name)
+}
+
+// GetClaims mocks base method.
+func (m *MockRepositoryInterface) GetClaims(tokenStr string, key []byte) (v5.MapClaims, bool) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetClaims", tokenStr, key)
+	ret0, _ := ret[0].(v5.MapClaims)
+	ret1, _ := ret[1].(bool)
+	return ret0, ret1
+}
+
+// GetClaims indicates an expected call of GetClaims.
+func (mr *MockRepositoryInterfaceMockRecorder) GetClaims(tokenStr, key interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetClaims", reflect.TypeOf((*MockRepositoryInterface)(nil).GetClaims), tokenStr, key)
+}
+
+// GetProfile mocks base method.
+func (m *MockRepositoryInterface) GetProfile(ctx context.Context, user_id int) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetProfile", ctx, user_id)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// GetProfile indicates an expected call of GetProfile.
+func (mr *MockRepositoryInterfaceMockRecorder) GetProfile(ctx, user_id interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetProfile", reflect.TypeOf((*MockRepositoryInterface)(nil).GetProfile), ctx, user_id)
+}
+
 // GetTestById mocks base method.
 func (m *MockRepositoryInterface) GetTestById(ctx context.Context, input GetTestByIdInput) (GetTestByIdOutput, error) {
 	m.ctrl.T.Helper()
@@ -47,4 +107,48 @@ func (m *MockRepositoryInterface) GetTestById(ctx context.Context, input GetTest
 func (mr *MockRepositoryInterfaceMockRecorder) GetTestById(ctx, input interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetTestById", reflect.TypeOf((*MockRepositoryInterface)(nil).GetTestById), ctx, input)
+}
+
+// RefreshToken mocks base method.
+func (m *MockRepositoryInterface) RefreshToken(token string) (string, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "RefreshToken", token)
+	ret0, _ := ret[0].(string)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// RefreshToken indicates an expected call of RefreshToken.
+func (mr *MockRepositoryInterfaceMockRecorder) RefreshToken(token interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RefreshToken", reflect.TypeOf((*MockRepositoryInterface)(nil).RefreshToken), token)
+}
+
+// SignJWTToken mocks base method.
+func (m *MockRepositoryInterface) SignJWTToken(cl v5.MapClaims, key []byte) (string, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "SignJWTToken", cl, key)
+	ret0, _ := ret[0].(string)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// SignJWTToken indicates an expected call of SignJWTToken.
+func (mr *MockRepositoryInterfaceMockRecorder) SignJWTToken(cl, key interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SignJWTToken", reflect.TypeOf((*MockRepositoryInterface)(nil).SignJWTToken), cl, key)
+}
+
+// UpdateProfile mocks base method.
+func (m *MockRepositoryInterface) UpdateProfile(ctx context.Context, user_id int) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "UpdateProfile", ctx, user_id)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// UpdateProfile indicates an expected call of UpdateProfile.
+func (mr *MockRepositoryInterfaceMockRecorder) UpdateProfile(ctx, user_id interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "UpdateProfile", reflect.TypeOf((*MockRepositoryInterface)(nil).UpdateProfile), ctx, user_id)
 }
